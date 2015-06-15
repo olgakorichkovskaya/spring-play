e06b4439f276bb11a06166c9128946033fc37000
Dewy <olgakorichkovskaya@gmail.com>
2015-06-15 14:11:12 +0300

    condition bean

---
+++ src/main/java/base/conditional/MagicBean.java
@@ -0,0 +1,5 @@
+package base.conditional-;
+
+public class MagicBean {
+    
+}
---
+++ src/main/java/base/conditional/MagicExistsCondition.java
@@ -0,0 +1,7 @@
+package base.conditional;
+
+/**
+ * Created by Dewy on 15.06.2015.
+ */
+public class MagicExistsCondition {
+}
